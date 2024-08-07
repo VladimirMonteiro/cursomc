0ed9c212e3d21831bf4627559d6d8427e17aa609
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-08-07 20:22:36 -0300

    testing the first class of the domain

--- src/main/java/com/outercode/cursomc/controllers/CategoryController.java
+++ src/main/java/com/outercode/cursomc/controllers/CategoryController.java
@@ -1,16 +1,26 @@
 package com.outercode.cursomc.controllers;
 
 
+import com.outercode.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
     @GetMapping
-    public String listar(){
-        return "REST está funcionando";
+    public List<Category> listar(){
+        List<Category> lista = new ArrayList<>();
+        Category cat1 = new Category(1, "Informática");
+        Category cat2 = new Category(2, "Moda");
+
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 }
